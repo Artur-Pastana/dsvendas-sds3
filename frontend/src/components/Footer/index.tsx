e9d94290d56368e34d5f06a0f9dfcfb5e5043d03
Artur-Pastana <reitutu_15@hotmail.com>
2021-05-04 00:50:07 -0300

    Componente Statico Basico

---
+++ frontend/src/components/Footer/index.tsx
@@ -0,0 +1,15 @@
+
+const Footer = () => {
+    return (
+        <div>
+            <footer className="footer mt-auto py-3 bg-dark">
+                <div className="container">
+                    <p className="text-light">Aplicativo desenvolvido por <a href="https://github.com/Artur-Pastana" target="_blank" rel="noreferrer">Artur Da Glória</a></p>
+                    <p className="text-light"><small><strong>Spring React - sds3</strong><br />
+      Evento da DevSuperior: <a href="https://instagram.com/devsuperior.ig" target="_blank" rel="noreferrer">@devsuperior.ig</a></small></p>
+                </div>
+            </footer>
+        </div>
+    );
+}
+export default Footer;
